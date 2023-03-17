fab313e7f4d84ab86eb6c996650f0826b981a157
Gokul <gokulgamer9404@gmail.com>
2023-03-18 01:37:35 +0530

    updated the script to get some info

--- rename.py
+++ rename.py
@@ -7,7 +7,9 @@ new_path = "I:\\Internship\\Data_Annotation\\Final"
 
 fldr_lst = os.listdir(path)
 fldr_lst = [x for x in fldr_lst if x.split(".")[-1] != 'xlsx']
+fldr_lst.remove('New folder')
 
+count = 1
 for folder in fldr_lst:
     fldr_path = os.path.join(path, folder)
     new_fldr_path = os.path.join(new_path, folder)
@@ -15,15 +17,16 @@ for folder in fldr_lst:
     image_no = 1
     os.mkdir(new_fldr_path)
     for image in images_path:
-        # cv2.imread()
-        # cv2.imwrite(f"{Video_folder}\{dst} frame\\Img_{x}.jpg", frame)
         read_path = os.path.join(fldr_path, image)
         
         img = f"image_{image_no}.png"
-        image_no+=1
         write_path = os.path.join(new_fldr_path, img)
         Img = cv2.imread(read_path)
         
+        image_no+=1
+        count+=1
+        
         print(read_path,write_path)
-
         cv2.imwrite(write_path, Img)
+
+print(count)
